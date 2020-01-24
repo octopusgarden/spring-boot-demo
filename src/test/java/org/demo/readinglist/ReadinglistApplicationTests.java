13f77e2fb2256ac63a08ff59126a9071e11eda95
elisa.natalie <elisa.natalie@smaato.com>
2020-01-25 00:31:08 +0100

    Spring Boot in Action chapter two

---
+++ src/test/java/org/demo/readinglist/ReadinglistApplicationTests.java
@@ -0,0 +1,12 @@
+package org.demo.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinglistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
